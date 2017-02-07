1105004b11af2285591e6512412e11b8675d9462
Curtis Frifeldt <cfrifel@hawaii.edu>
2017-02-06 23:17:59 -1000

    WOD: ziplist

---
+++ ziplist.js
@@ -0,0 +1,20 @@
+/*
+ * Created by cfrif on 2/6/2017.
+ */
+
+const one = [1, 3, 5, 7, 9];
+const two = [2, 4, 6, 8, 10];
+
+function zipList(one, two) {
+  const result = [];
+  for (let i = 0; i < one.length; i++) {
+    result.push(one[i], two[i]);
+  }
+  return result;
+}
+console.log(zipList(one, two));
+
+function zipListTheSimpleWay(one, two) {
+  return _.flatten(_.zip(one, two));
+}
+console.log(zipListTheSimpleWay(one, two));
